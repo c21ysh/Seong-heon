d1ed2f1bbaef0263ace3c5e505b91d0b96e6c23c
302-29 <302-29@DESKTOP-VSF8P9Q>
2022-10-12 18:23:03 +0900

    ㅋㅋ

--- ch03Ex/src/ch03Ex/Ex03_18.java
+++ ch03Ex/src/ch03Ex/Ex03_18.java
@@ -3,8 +3,18 @@ package ch03Ex;
 public class Ex03_18 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int a=10,b=5,c=3,d=1;
+		a = a+3;
+		b = b-4;
+		c = c*3;
+		d = d/3;
+		d = d%2;
+		//복합대입 연산자로 변경
+		a += 3;
+		b -= 4;
+		c *= 3;
+		d /= 3;
+		d %= 2;
 	}
 
 }
